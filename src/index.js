1ed8ddc28ac83e08ce55d8818b9a3bf0d8ca59a1
rifqifam <muhammad.rifqi.fameizy@mail.ugm.ac.id>
2022-11-16 12:06:52 +0700

    slicing 1

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+// window.addEventListener('scroll', () => {
+//     // console.log(window.scrollY);
+//     if (window.scrollY >= 50) {
+//         nav.classList.add('bgcolor');
+//     } else {
+//         nav.classList.remove('bgcolor');
+//     }
+
+// })
+// $window().scroll(function(){
+//     $('nav').toggleClass('scrolled',$(this).scrollTop()>50);
+// });
+
